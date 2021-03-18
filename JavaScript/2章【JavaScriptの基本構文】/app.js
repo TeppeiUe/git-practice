53fd024356ac980befd74b600c34c7a42db86626
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-03-18 23:56:41 +0900

    [Start] JavaScriptを学ぼう

---
+++ JavaScript/2章【JavaScriptの基本構文】/app.js
@@ -0,0 +1,49 @@
+// var hello='Hello World';
+// var int1=1;
+// var int2=25;
+// var float=3.14;
+// var str='JavaScriptを覚えよう';
+
+// alert(hello+" & "+str);
+
+
+// var orange=100;
+// var apple=120;
+
+// if (orange < apple){
+//   alert("みかんの値段がりんごより安い");
+// }else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// }else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+
+// var max=100;
+// var num=1;
+// var count=0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+// alert('2の' + count + '乗で' + max + 'を超えます');
+
+
+var num = 1;
+var fuctrial = 1;
+
+do{
+  fuctrial = fuctrial * num;
+  num ++;
+}while(num <= 5);
+alert('5! = ' + fuctrial);
+
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+// alert('1から10まで足した結果は' + num + 'です');
