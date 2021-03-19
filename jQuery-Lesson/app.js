a3b3e4e5cc0ef53900cff7652b34c924475c3b6b
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-03-20 01:37:45 +0900

    [Finish] 9-5

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.box1').slideDown(1000);
+  $('.box2').slideUp(1000);
+  $('.box3').show(1000);
+  $('.box4').hide(1000);
+});
+
+// $(function(){
+//   $('.kadai').slideDown(function(){
+//     $('.kadai').css({
+//       'height':'100px',
+//       'background-color':'#00f'
+//     }).slideUp();
+//   });
+// });
