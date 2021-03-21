eda30bead8e1bec0c72622c199082e40198e42ac
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-03-21 11:10:05 +0900

    [Finish] 9-8

---
+++ jQuery-Lesson/9-8/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition : 'slide',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  navType : 'bubble',
+  childrenElementType : 'div',
+  arrows : false,
+  autoPlay : true,
+  autoPlayDuration : 3000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
